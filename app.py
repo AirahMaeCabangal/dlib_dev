32f250b5e6c246c7bb927ee6bfa85a603b68bac4
AirahMaeCabangal <102496852+AirahMaeCabangal@users.noreply.github.com>
2023-12-13 23:23:39 +0800

    app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 class VideoProcessor:
     def drawPoints(image, faceLandmarks, startpoint, endpoint, isClosed=False):
-    points = []
+        points = []
     for i in range(startpoint, endpoint+1):
         point = [faceLandmarks.part(i).x, faceLandmarks.part(i).y]
         points.append(point)
